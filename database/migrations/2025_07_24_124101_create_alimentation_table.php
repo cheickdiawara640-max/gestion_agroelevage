b1f16d272246fcd1a9b5b334a0c413edf4cb3b19
cheickdiawara640-max <cheickdiawara640@gmail.com>
2025-12-29 14:05:29 +0000

    Update 2025_07_24_124101_create_alimentation_table.php

--- database/migrations/2025_07_24_124101_create_alimentation_table.php
+++ database/migrations/2025_07_24_124101_create_alimentation_table.php
@@ -10,14 +10,13 @@ return new class extends Migration
     {
         Schema::create('alimentations', function (Blueprint $table) {
             $table->id();
-            // C'est ici que l'erreur doit être : vérifie bien les parenthèses et le point-virgule à la fin
             $table->foreignId('animal_id')->nullable()->constrained()->onDelete('set null');
             $table->string('type_aliment');
             $table->integer('quantite');
             $table->date('date_distribution');
             $table->timestamps();
-        }); // Vérifie bien qu'il y a ce ");" ici
-    
+        });
+    } // <-- Cette accolade manquait ici !
 
     public function down(): void
     {
